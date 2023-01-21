7bbc1dd2d34cf78d224e12d1a54cdc83b74d9d7f
disamiron <denis.mironov@ddgcorp.ru>
2023-01-21 17:18:45 +0300

    update app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterPageComponent } from './modules/pages/character-page/character-page.component';
+import { CharactersListComponent } from './modules/pages/characters-list/characters-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CharactersListComponent,
+  },
+  {
+    path: ':characterId',
+    component: CharacterPageComponent,
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
